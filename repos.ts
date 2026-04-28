113e825703b6c8d4e5495ac5d3a9a31a4444590c
taisan <131413638+taisan11@users.noreply.github.com>
2026-04-28 17:44:36 +0900

    Update repos.ts

--- repos.ts
+++ repos.ts
@@ -12,9 +12,24 @@ export interface Repos {
 export const repos: Repos = {
   "honojs/website": {
     gitUrl: "https://github.com/honojs/website.git",
-    path: ["docs"],
+    path: ["docs","examples"],
     branch:"main",
     license: "MIT",
+    lang:"en"
+  },
+  "akku1139/hono-ja": {
+    gitUrl: "https://github.com/akku1139/hono-ja.git",
+    path: ["docs","examples"],
+    branch:"main",
+    license: "MIT",
+    lang:"ja"
+  },
+  "sircmpwn/wayland-book": {
+    gitUrl:"https://git.sr.ht/~sircmpwn/wayland-book.git",
+    path:["src"],
+    branch:"master",
+    license:"CC BY-SA 4.0",
+    licenseUrl:"http://creativecommons.org/licenses/by-sa/4.0/",
     lang:"en"
   }
 }
